6ffe5e71442d24770cd1af36d14cc42105860d39
monika-sasikumar <147509522+monika-sasikumar@users.noreply.github.com>
2024-07-29 20:09:25 +0530

    variables.java
    Learning variables in Java like Integer and String.

---
+++ variables.java
@@ -0,0 +1,11 @@
+import  java.lang.System;
+class variables
+{
+    public static void main(String args[])
+    {
+        int container=67;
+        String container2="John";
+        System.out.println(container);
+        System.out.print(container2);
+    }
+}
