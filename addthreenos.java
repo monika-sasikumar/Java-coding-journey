f5801218ff637ddddd92aebae10cb64a0d00bed2
monika-sasikumar <147509522+monika-sasikumar@users.noreply.github.com>
2024-08-06 20:21:44 +0530

    Add files via upload

---
+++ addthreenos.java
@@ -0,0 +1,12 @@
+// adding three nos
+
+import java.lang.System;
+class add3{
+    public static void main(String args[ ])
+    {
+        int a=10;
+        int b=20;
+        int c=40;
+        System.out.println(a+b+c);
+    }
+}
